76c5263e2d6e8782e51bc8c886132489a84a7049
FatinHaib <119875167+FatinHaib@users.noreply.github.com>
2025-07-01 14:44:09 +0300

    Add files via upload

---
+++ menuOption.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class MenuOption(Enum):
+    SAVE = 1
+    SEARCH_ID = 2
+    PRINT_AGES_AVG = 3
+    PRINT_NAMES = 4
+    PRINT_IDS = 5
+    PRINT_ALL = 6
+    PRINT_BY_IDX = 7
+    SAVE_DATA = 8 
+    EXIT = 9
